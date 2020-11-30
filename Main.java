62d542374f1d3093864aa4ecbee5f9f96f495791
azoam <sazouzi21@gmail.com>
2020-11-30 21:19:43 +0000

    more testing fun

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello Worrld!");
         System.out.println("Hello New Branch!");
+        System.out.println("Hello New Test");
     }
 }
